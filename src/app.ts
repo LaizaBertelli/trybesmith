bd55eecf98974613872fc12533276d3cc7be702a
LaizaBertelli <laizabertelli@hotmail.com>
2022-03-29 15:08:46 -0300

    feat: add routes to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import errorHandler from './middlewares/error.middleware';
+import productRouter from './routes/products.routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(errorHandler);
+app.use(productRouter);
 
 export default app;
