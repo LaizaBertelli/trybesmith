b3d635d23aa6aa156fd5307b3c69a23e50e2f30f
LaizaBertelli <laizabertelli@hotmail.com>
2022-03-29 15:07:14 -0300

    feat: implement product service

---
+++ src/services/product.service.ts
@@ -0,0 +1,21 @@
+import ProductModel from '../models/product.model';
+import connection from '../models/connection';
+import Product from '../interfaces/product.interface';
+
+export default class ProductService {
+  public model: ProductModel;
+  
+  constructor() {
+    this.model = new ProductModel(connection);
+  }
+
+  public async getAll(): Promise<Product[]> {
+    const products = await this.model.getAll();
+    return products;
+  }
+
+  public async create(product: Product): Promise<Product> {
+    const newProduct = await this.model.create(product);
+    return newProduct;
+  }
+}
