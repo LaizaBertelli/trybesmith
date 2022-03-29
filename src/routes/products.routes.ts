1796e982a55f973baa18ee7fcd67d078547e020e
LaizaBertelli <laizabertelli@hotmail.com>
2022-03-29 15:08:23 -0300

    feat: implement product router

---
+++ src/routes/products.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import ProductController from '../controllers/product.controller';
+
+const productRouter = Router();
+
+const productController = new ProductController();
+
+productRouter.get('/products', productController.getAll);
+productRouter.post('/products', productController.create);
+
+export default productRouter;
