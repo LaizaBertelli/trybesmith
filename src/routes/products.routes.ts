db10dc0744ee351eaeb0a1fed9b00f419439744a
LaizaBertelli <laizabertelli@hotmail.com>
2022-03-29 16:02:55 -0300

    feat: add validation to router

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import ProductController from '../controllers/product.controller';
+import validateProduct from '../middlewares/product.middleware';
 
 const productRouter = Router();
 
 const productController = new ProductController();
 
 productRouter.get('/products', productController.getAll);
-productRouter.post('/products', productController.create);
+productRouter.post('/products', validateProduct, productController.create);
 
 export default productRouter;
