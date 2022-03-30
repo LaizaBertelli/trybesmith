4fe7f73427fdde035cd41fd38c7ff4c64d5f88bd
LaizaBertelli <laizabertelli@hotmail.com>
2022-03-30 02:49:38 -0300

    feat: removes error handler

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,20 +1,7 @@
-import { Request, Response, NextFunction } from 'express';
+// import { Request, Response, NextFunction } from 'express';
 
-export default function errorHandler(err: Error, _req: Request, res: Response, next: NextFunction) {
-  const { name, message } = err;
-  console.log(`name: ${name}`);
-
-  switch (name) {
-    case 'NotFoundError':
-      res.status(404).json({ message });
-      break;
-    case 'ConflitError':
-      res.status(409).json({ message });
-      break;
-    default:
-      console.error(err);
-      res.sendStatus(500);
-  }
-
-  next();
-}
+// export default function errorHandler(err: Error, _req: Request, res: Response, next: NextFunction) {
+//   console.log(err);
+  
+//   next();
+// }
