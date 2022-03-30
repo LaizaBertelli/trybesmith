2c6829abaee7b21fce830808a31b5309986b06ac
LaizaBertelli <laizabertelli@hotmail.com>
2022-03-30 02:49:10 -0300

    feat: change product validation types

--- src/middlewares/product.middleware.ts
+++ src/middlewares/product.middleware.ts
@@ -1,11 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
-import Product from '../interfaces/product.interface';
 import ProductSchema from '../schemas/product.schema';
 
 export default async function validateProduct(req: Request, res: Response, next: NextFunction) {
-  const product: Product = req.body;
-  const { error } = ProductSchema.validate(product);
-
+  const { error } = ProductSchema.validate(req.body);
+  
   if (error) {
     const errorDetailed = error.details[0].message.split('|');
     return res.status(parseInt(errorDetailed[0], 10)).json({ error: errorDetailed[1] });
