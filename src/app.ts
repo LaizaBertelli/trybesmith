085ae049f3e2662c3a941ac448d43615fe8d2f81
LaizaBertelli <laizabertelli@hotmail.com>
2022-03-30 02:48:28 -0300

    feat: add new router to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import errorHandler from './middlewares/error.middleware';
-import productRouter from './routes/products.routes';
+// import errorHandler from './middlewares/error.middleware';
+import router from './routes';
+import 'express-async-errors';
 
 const app = express();
 
 app.use(express.json());
-app.use(errorHandler);
-app.use(productRouter);
+// app.use(errorHandler);
+app.use(router);
 
 export default app;
