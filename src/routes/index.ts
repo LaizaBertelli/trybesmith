82318b0955f0f36aaae845eb6d72472f8b7357dd
LaizaBertelli <laizabertelli@hotmail.com>
2022-04-03 19:27:26 -0300

    feat: add login router to index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
-import orderRouter from './order.route';
 
+import orderRouter from './order.route';
 import productRouter from './products.routes';
 import userRouter from './user.route';
+import loginRouter from './login.route';
 
 const router = Router();
 
 router.use('/products', productRouter);
 router.use('/users', userRouter);
 router.use('/orders', orderRouter);
+router.use('/login', loginRouter);
 
 export default router;
